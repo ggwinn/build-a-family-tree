4c1bfaa298e91e617d4692be65baa4c3f54cda2f
geannagwinn <you@example.com>
2026-01-26 11:14:33 -0500

    modal fix

--- index.js
+++ index.js
@@ -1,26 +1,11 @@
-const btns = document.querySelectorAll("[data-target]");
-const close_modals = document.querySelectorAll(".close-modal");
-const overlay = document.getElementById("overlay");
+const open = document.getElementById('open');
+const modal_container = document.getElementById('modal_container');
+const close = document.getElementById("close");
 
-btns.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    document.querySelector(btn.dataset.target).classList.add("active");
-    overlay.classList.add("active");
-  });
-});
+open.addEventListen('click', () => {
+  modal_container.classList.add("show");
+})
 
-close_modals.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    const modal = btn.closest(".modal");
-    modal.classList.remove("active");
-    overlay.classList.remove("active");
-  });
-});
-
-window.onclick = (event) => {
-  if (event.target == overlay) {
-    const modals = document.querySelectorAll(".modal");
-    modals.forEach((modal) => modal.classList.remove("active"));
-    overlay.classList.remove("active");
-  }
-};
+open.addEventListen('click', () => {
+  modal_container.classList.remove("show");
+})
