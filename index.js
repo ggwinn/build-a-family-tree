a245a19bf12468011a195bc3dcabb919f1b4aea6
geannagwinn <you@example.com>
2026-01-26 14:16:20 -0500

    modal fix

--- index.js
+++ index.js
@@ -1,26 +1,6 @@
-const btns = document.querySelectorAll("[data-target]");
-const close_modals = document.querySelectorAll(".close-modal");
-const overlay = document.getElementById("overlay");
+const enrollButton = document.querySelector('#enroll-button');
+const enrollDialog = document.querySelector('#enroll-dialog');
 
-btns.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    document.querySelector(btn.dataset.target).classList.add("active");
-    overlay.classList.add("active");
-  });
-});
-
-close_modals.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    const modal = btn.closest(".modal");
-    modal.classList.remove("active");
-    overlay.classList.remove("active");
-  });
-});
-
-window.onclick = (event) => {
-  if (event.target == overlay) {
-    const modals = document.querySelectorAll(".modal");
-    modals.forEach((modal) => modal.classList.remove("active"));
-    overlay.classList.remove("active");
-  }
-};
+enrollButton.addEventListener('click', () => {
+  enrollDialog.show();
+})
